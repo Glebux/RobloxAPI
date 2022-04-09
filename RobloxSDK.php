359bd866714ec3181bbf93ca6c848e2ecbd71e04
Glebux <glebpashkevich1@gmail.com>
2022-04-09 22:52:05 +0300

    Added game class and password protection for cookies, getToken and getAuthTicket functions

--- RobloxSDK.php
+++ RobloxSDK.php
@@ -24,7 +24,7 @@
         }
         return $headers;
     }
-    function makerobloxrequest(string $url, ?bool $post, ?array $postfields, ?string $contenttype, ?string $cookie, ?string $token, ?array $headers, ?bool $returnheaders){
+    function makerobloxrequest(string $url, bool|string $request = false, ?array $postfields = null, ?string $contenttype = null, ?string $cookie = null, ?string $token = null, ?array $headers = null, ?bool $returnheaders = false, ?bool $returncode = false){
         switch ($contenttype){
             case "json":
                 $contenttype = "application/json";
@@ -35,9 +35,16 @@
             default:
                 $contenttype = "application/json";
         }
+        $post = false;
         $ch = curl_init($url);
         if (isset($cookie)){
             curl_setopt($ch, CURLOPT_COOKIE, $cookie);
+        }
+        if (gettype($request) == "bool"){
+            $post = $request;
+        }
+        else{
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $request);
         }
         curl_setopt($ch, CURLOPT_POST, $post);
         if (isset($postfields)){
@@ -67,15 +74,19 @@
             $header = substr($resp, 0, $header_size);
             return headersToArray($header);
         }
+        if ($returncode == true){
+            return curl_getinfo($ch, CURLINFO_RESPONSE_CODE);
+        }
         return $resp;
     }
     class RobloxClient{
-        protected string $cookie;
-        protected string $cookieheader;
+        private string $cookie;
+        private string $cookieheader;
         public string $userid;
         public string $username;
         public string $displayname;
-        public function __construct(string $cookie)
+        protected string $accesspassword = "";
+        public function __construct(string $cookie, $accesspassword = "")
         {
             $this->cookie = $cookie;
             $this->cookieheader = ".ROBLOSECURITY=" . $this->cookie . "; Path=/; Domain=.www.roblox.com; HttpOnly; Expires=Mon, 1 Jan " . strval(intval(date("Y") + 50)) . " 00:00:00 GMT";
@@ -83,14 +94,48 @@
             if (!isset($data["id"])){
                 throw new ErrorException("Identification failed or invalid cookie");
             }
+            if (isset($accesspassword)){
+                $this->accesspassword = $accesspassword;
+            }
             $this->userid = $data["id"];
             $this->username = $data["name"];
             $this->displayname = $data["displayName"];
         }
-        public function gettoken(): string{
+        public function getcookie(string $password = ""){
+            if ($password == $this->accesspassword){
+                return $this->cookie;
+            }
+            return "Incorrect password";
+        }
+        public function getcookieheader(string $password = ""){
+            if ($password == $this->accesspassword){
+                return $this->cookieheader;
+            }
+            return "Incorrect password";
+        }
+        public function setpassword(string $oldpassword, string $newpassword){
+            if ($this->accesspassword == $oldpassword){
+                $this->accesspassword = $newpassword;
+                return true;
+            }
+            return "Old password is incorrect";
+        }
+        public function gettokenbypass(string $password = ""){
+            if ($this->accesspassword == $password){
+                return $this->gettoken();
+            }
+            return "Incorrect password";
+        }
+        private function gettoken(): string{
             return makerobloxrequest("https://auth.roblox.com/v2/logout", true, null, null, $this->cookieheader, null, null, true)["x-csrf-token"];
         }
-        public function getauthticket(): string{
+        public function getauthticketbypass(string $password = ""){
+            if ($this->accesspassword == $password){
+                return $this->getauthticket();
+            }
+            return "Incorrect password";
+        }
+        private function getauthticket(): string{
             return substr(makerobloxrequest("https://auth.roblox.com/v1/authentication-ticket/", true, null, null, $this->cookieheader, $this->gettoken(), null, true)["rbx-authentication-ticket"], 1);
         }
         public function joinplaceid($placeid){
@@ -155,6 +200,15 @@
             }
             return false;
         }
+        public function isfriendswith(int $userid){
+            $data = $this->getfriends();
+            foreach ($data as $v){
+                if ($v->userid == $userid){
+                    return true;
+                }
+            }
+            return false;
+        }
     }
     class RobloxPlayer{
         public int $userid;
@@ -195,6 +249,108 @@
         }
         public function declinefriendrequest(RobloxClient $client){
             $client->declinefriendrequest($this->userid);
+        }
+        public function isfriendswith(int $userid){
+            $data = $this->getfriends();
+            foreach ($data as $v){
+                if ($v->userid == $userid){
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+    class RobloxGame{
+        public $gameid;
+        public $placeid;
+        public $universeid;
+        public $placename;
+        public $description;
+        public $creator;
+        public $price;
+        public $created;
+        public $updated;
+        public $playing;
+        public $visits;
+        public $maxplayers;
+        public function getuniversalid(): int{
+            return json_decode(makerobloxrequest("https://api.roblox.com/universes/get-universe-containing-place?placeid=".$this->gameid, false, null, null, null, null, null, false), true)["UniverseId"];
+        }
+        public function __construct(int $placeid){
+            $this->gameid = $placeid;
+            $this->universeid = $this->getuniversalid();
+            if (!isset($this->universeid)){
+                throw new ErrorException("Invalid game or cannot fetch information");
+            }
+            $data = json_decode(makerobloxrequest("https://games.roblox.com/v1/games?universeIds=".$this->getuniversalid(), false, null, null, null, null, null, null), true)["data"][0];
+            $this->placeid = $data["rootPlaceId"];
+            $this->placename = $data["name"];
+            $this->description = $data["description"];
+            $this->creator = new RobloxPlayer($data["creator"]["id"]);
+            switch ($data["price"]){
+                case null:
+                    $this->price = 0;
+                default:
+                    $this->price = $data["price"];
+            }
+            $this->created = $data["created"];
+            $this->updated = $data["updated"];
+            $this->playing = $data["playing"];
+            $this->visits = $data["visits"];
+            $this->maxplayers = $data["maxPlayers"];
+        }
+        public function getservers(?RobloxClient $client = null, ?string $password = ""){
+            $data = null;
+            if (isset($client) and isset($password)){
+                $data = json_decode(makerobloxrequest("https://games.roblox.com/v1/games/".$this->gameid."/servers/Public?sortOrder=Asc&limit=100", false, null, null, $client->getcookieheader($password), $client->gettokenbypass($password), null, false), true);
+            }
+            else{
+                $data = json_decode(makerobloxrequest("https://games.roblox.com/v1/games/".$this->gameid."/servers/Public?sortOrder=Asc&limit=100", false, null, null, null, null, null, false), true);
+            }
+            $servers = array();
+            foreach ($data["data"] as $v) {
+                $playertokens = array();
+                foreach ($v["playerTokens"] as $p) {
+                    $playertokens[] = $p;
+                }
+                //print_r($players);
+                $ping = 0;
+                if (isset($v["ping"])){
+                    $ping = $v["ping"];
+                }
+                $serverdata = array(
+                    "jobId" => $v["id"],
+                    "playing" => $v["playing"],
+                    "playerTokens" => $playertokens,
+                    "ping" => $ping,
+                    "fps" => $v["fps"]
+                );
+                if (isset($client) and isset($password)){
+                    foreach ($v["players"] as $p){
+                        $serverdata["players"] = new RobloxPlayer($p["id"]);
+                    }
+                }
+                $servers[] = $serverdata;
+            }
+            return $servers;
+        }
+        public function getplaces(){
+            $data = json_decode(makerobloxrequest("https://develop.roblox.com/v1/universes/".$this->universeid."/places?sortOrder=Asc&limit=100"), true);
+            $places = array();
+            foreach ($data["data"] as $v){
+                $places[] = new RobloxGame($v["id"]);
+            }
+            return $places;
+        }
+        public function setname(RobloxClient $client, string $password, string $name){
+            $request = makerobloxrequest("https://develop.roblox.com/v1/places/".$this->gameid, true, array("name" => $name, "description" => $this->description), null, $client->getcookieheader($password), $client->gettokenbypass($password), null, false, true);
+            if ($request == 200){return true;}
+            return false;
+        }
+        public function setdescription(RobloxClient $client, string $password, string $description){
+            $request = makerobloxrequest("https://develop.roblox.com/v1/places/".$this->gameid, true, array("name" => $this->placename, "description" => $description), null, $client->getcookieheader($password), $client->gettokenbypass($password), null, false, true);
+            if ($request == 200){return true;}
+            return false;
         }
     }
 ?>
